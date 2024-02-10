ec977aa8096694e2a0fc1a5441e00d0b01792b25
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-02-10 15:33:17 +0100

    Setup App as component element

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import Home from "./pages/Home";
 import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        errorElement: <ErrorPage />,
+      },
+    ],
   },
 ]);
 
