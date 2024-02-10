9a1d6c2ead994999ba7d278554a68adde92141af
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-02-10 18:39:49 +0100

    Home: Ajout composant Row

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Main from "../components/Main";
+import Row from "../components/Row";
+import requests from "../Requests";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Main />
-    </div>
+      <Row rowID="1" title="Upcoming" fetchUrl={requests.requestUpcoming} />
+      <Row rowID="2" title="Popular" fetchUrl={requests.requestPopular} />
+      <Row rowID="3" title="Trending" fetchUrl={requests.requestTrending} />
+      <Row rowID="4" title="Top Rated" fetchUrl={requests.requestTopRated} />
+      <Row rowID="5" title="Horror" fetchUrl={requests.requestHorror} />
+    </>
   );
 };
 
